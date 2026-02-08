4bcf763028e6004c18c0b483bb05e5537b9fbd64
ManikumarJ <manikumarj6@gmail.com>
2026-02-08 13:34:13 +0530

    updates

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -14,15 +14,9 @@ const Products = () => {
       <div className="max-w-7xl mx-auto flex flex-wrap justify-center gap-8">
         {products.map((product) => (
           <div
-            key={product.id}
-            className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden flex flex-col w-72"
-          >
+            key={product.id} className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden flex flex-col w-72">
             <div className="h-56 overflow-hidden">
-              <img
-                src={product.image}
-                alt={product.name}
-                className="w-full h-full object-cover hover:scale-105 transition duration-300"
-              />
+              <img src={product.image} alt={product.name}  className="w-full h-full object-cover hover:scale-105 transition duration-300" />
             </div>
 
             <div className="p-5 flex flex-col flex-grow">
@@ -30,6 +24,7 @@ const Products = () => {
               <p className="text-sm text-gray-500 mb-1">Category: {product.category}</p>
               <p className="text-yellow-500 text-sm mb-2"> {product.rating}</p>
               <p className="text-xl font-bold text-[#064232] mb-3">₹{product.price}</p>
+              
               <button className="mt-auto bg-[#0F2A1D] text-white py-2 rounded-lg hover:bg-[#9AE6B4] hover:text-[#0F2A1D] transition duration-300 font-medium">
                 Add to Cart
               </button>
